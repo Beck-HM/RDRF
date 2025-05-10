f620703308df07c3fc533ee162b71ebef1965a2a
Beck <bakerlk0307@gmail.com>
2025-05-10 00:00:00 -0800

    RDRF.Cli: cross-platform CLI with backup + res commands

---
+++ src/RDRF.Cli/Program.cs
@@ -0,0 +1,14 @@
+﻿using RDRF.Cli.Commands;
+using System.CommandLine;
+
+class Program
+{
+    static async Task<int> Main(string[] args)
+    {
+        var root = new RootCommand("RDRF - Resilient Distributed Replication Format");
+        root.Subcommands.Add(new BackupCommand());
+        root.Subcommands.Add(new RestoreCommand());
+        var parseResult = root.Parse(args);
+        return await parseResult.InvokeAsync();
+    }
+}
